5d7b95852735c842b7c42bc15e09d2fe0157aa4b
aischwa <aischwa@umich.edu>
2016-02-20 16:41:23 -0500

    added error page...still need to fix video loading problem in safari and favicon loading problem in chrome before optimizing for mobile

--- main site/routes.py
+++ main site/routes.py
@@ -5,15 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html', name='index', title='Welcome')
+
+@app.route('/index')
+def home():
+  return render_template('index.html', name='index', title='Welcome')
 
 @app.route('/client')
 def client():
-  return render_template('client.html')
+  return render_template('client.html', name='client', title='Client Information')
 
 @app.route('/student')
 def student():
-  return render_template('student.html')
+  return render_template('student.html', name='student', title='Student Information')
 
 # @app.route('/q8/<name>')
 # def q8(name):
@@ -21,11 +25,11 @@ def student():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('index.html', name="index", title="HOME"), 404
+    return render_template('error.html', name="error", title="404"), 404
 
 @app.errorhandler(500)
 def page_not_found(e):
-    return render_template('index.html', name="index", title="HOME"), 500
+    return render_template('error.html', name="error", title="500"), 500
 
 
 if __name__ == '__main__':
